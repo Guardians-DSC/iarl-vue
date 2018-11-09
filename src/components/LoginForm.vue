f22eb0b1b59696e27e01e542e98e2652e415d8ae
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2018-11-09 20:46:52 -0300

    Adiciona validação no form de login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="login-form">
-    <input type="text" class="username" placeholder="username" v-model="login.username">
+  <form class="login-form" @submit.prevent="submit()">
+    <input ref="username" type="text" class="username" placeholder="username" v-model="login.username">
     <input type="password" class="password" placeholder="password" v-model="login.password">
-    <button @click="submit()">Login</button>
-  </div>
+    <div class="error" v-if="error">* {{error}}</div>
+    <button type="submit" :class="{ disabled: submitDisabled }" :disabled="submitDisabled" >Login</button>
+  </form>
 </template>
 
 <script>
@@ -13,10 +14,16 @@ import { mapMutations } from 'vuex'
 export default {
   data () {
     return {
+      error: '',
       login: {
         username: '',
         password: ''
       }
+    }
+  },
+  computed: {
+    submitDisabled () {
+      return !this.login.username || !this.login.password
     }
   },
   methods: {
@@ -27,7 +34,13 @@ export default {
           this.updateToken(res.data.token)
           this.$router.push('FileManager')
         })
+        .catch(err => {
+          this.error = err.response.data.error
+        })
     }
+  },
+  mounted () {
+    this.$refs.username.focus()
   }
 }
 </script>
@@ -42,14 +55,27 @@ input {
   padding: 0.5rem 1rem;
   border-radius: 5px;
   border: 1px solid #b1b1b1;
+  &[type=password] {
+    margin-bottom: 1rem;
+  }
 }
 button {
-  margin-top: 1rem;
+  margin-top: .5rem;
   padding: 0.5rem;
   background: #33c7c7;
   color: #fff;
   border: 1px solid transparent;
   border-radius: 5px;
   cursor: pointer;
+}
+.disabled {
+  background-color: #ccc;
+  color: #999;
+}
+.error {
+  color: #d63939;
+  font-size: 12px;
+  font-weight: bold;
+  margin: 0 .5rem;
 }
 </style>
